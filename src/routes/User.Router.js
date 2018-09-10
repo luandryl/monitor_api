26aa91dd17156fe44ae5747a880df9189a3c77f5
Luan Andryl <luan.andryl@gmail.com>
2018-09-10 02:54:57 -0300

    protect middleware add to user route

--- src/routes/User.Router.js
+++ src/routes/User.Router.js
@@ -10,28 +10,32 @@ let router = express.Router()
 */
 let us = new UserController()
 
+import passport from 'passport'
+const protect = passport.authenticate('jwt', {
+	session: false
+})
 
 /*
   routing the controller object through student resource endpoints
 */
-router.get('/', (req, res) => {
+router.get('/', protect, (req, res) => {
   res.send({
     response: "hellow"
   })
 })
-router.post('/', (req, res) => {
+router.post('/', protect, (req, res) => {
   us.save(req, res)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', protect, (req, res) => {
   us.getById(req, res)
 })
  
-router.put('/:id', (req, res) => {
+router.put('/:id', protect, (req, res) => {
   us.updateById(req, res)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', protect, (req, res) => {
   us.removeById(req, res)
 })
 

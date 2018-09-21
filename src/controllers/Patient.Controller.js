bbfca1ff56b42850f32f8ec0e4d4e5d544254d43
Luan Andryl <luan.andryl@gmail.com>
2018-09-21 13:15:53 -0300

    get patients from doc id

--- src/controllers/Patient.Controller.js
+++ src/controllers/Patient.Controller.js
@@ -7,4 +7,21 @@ export default class UserController extends BaseController {
   constructor() {
     super(Patient)
   }
+
+  getByDoc (req, res) {
+    let data = {
+      _user: req.params.id
+    }
+    let patientPromise = new Patient().getByField(data)
+
+    Promise.all([
+      patientPromise
+    ]).then((data) => {
+      if (data) {
+        res.status(200).json(data[0]).end();
+      }
+    }).catch (err => {
+      console.log(err)
+    })
+  }
 }

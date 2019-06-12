6d7351f7ab2a85c7bf1c11b503bd2f77ecc491c6
Luan <luan.andryl@gmail.com>
2019-06-12 19:51:21 -0300

    database config

--- src/config/database.js
+++ src/config/database.js
@@ -20,7 +20,7 @@ export default class Database {
 
   local () {
     let connection
-    const localURI = 'mongodb://monitorv1_db:101520monitor@ds251362.mlab.com:51362/heroku_h42hbmvm'
+    const localURI = 'mongodb://heroku_mwv1mg5j:fahcs14kj8su6scml0m3r0glmp@ds237267.mlab.com:37267/heroku_mwv1mg5j'
     return connection = mongoose.connect(localURI, (err, db) => {
       if (err) console.log(err);
       if (mongoose.connection.readyState === 1)
